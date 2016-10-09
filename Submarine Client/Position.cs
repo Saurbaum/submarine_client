1ef5a3b4104d4da8d6fee9474c57485576ea55ac
Ian Newborn <saurbaum@gmail.com>
2016-10-09 22:34:12 +0100

    Initial commit

---
+++ Submarine Client/Position.cs
@@ -0,0 +1,14 @@
+﻿namespace Submarine_Client
+{
+	public class Position
+	{
+		public long X { get; set; }
+
+		public long Y { get; set; }
+
+		public override string ToString()
+		{
+			return $"{this.X}, {this.Y}";
+		}
+	}
+}
--- Submarine Client/Program.cs
+++ Submarine Client/Program.cs
@@ -1,15 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Submarine_Client
 {
+	using System;
+	using System.IO;
+	using System.Net;
 	class Program
 	{
+		private static readonly string host = "http://localhost";
 		static void Main(string[] args)
 		{
+			var seabedRequest = WebRequest.Create(host + "/seabed");
+			var pingRequest = WebRequest.Create(host + "/ping");
+			var locationRequest = WebRequest.Create(host + "/location");
+
+			var seaBed = JsonConvert.DeserializeObject<List<Position>>(GetRequestData(seabedRequest));
+
+			foreach (var s in seaBed)
+			{
+				Console.WriteLine(s);
+			}
+			Console.WriteLine(string.Empty);
+
+			Console.WriteLine(GetRequestData(pingRequest));
+			Console.WriteLine(string.Empty);
+
+			Console.WriteLine(JsonConvert.DeserializeObject<Position>(GetRequestData(locationRequest)));
+			Console.WriteLine(string.Empty);
+
+			int i = 0;
+		}
+
+		private static string GetRequestData(WebRequest request)
+		{
+			var response = request.GetResponse();
+
+			var dataStream = request.GetResponse().GetResponseStream();
+
+			if (dataStream != null)
+			{
+				var reader = new StreamReader(dataStream);
+
+				var data = reader.ReadToEnd();
+
+				reader.Close();
+				response.Close();
+
+				return data;
+			}
+
+			return string.Empty;
 		}
 	}
 }
